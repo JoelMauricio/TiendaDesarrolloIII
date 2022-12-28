9adfd3290d137adf81ff1079531dddfe7bcc5b7e
Joel <joeldavidmauriciohdez@gmail.com>
2022-12-27 21:09:05 -0400

    Se agregaron los foreign keys al query

--- DB_Queries/DSIIIstoreDB.sql
+++ DB_Queries/DSIIIstoreDB.sql
@@ -22,7 +22,9 @@ create table dbo.Product(
 	price DECIMAL(19,4) not null,
 	created_at datetime not null default getdate(),
 	last_modification datetime not null default getdate(),
-	deleted_state bit not null default 0
+	deleted_state bit not null default 0,
+	constraint FK_productCategory foreign key (category_id)
+	references dbo.Product_Category(category_id)
 )
 go
 
@@ -32,7 +34,9 @@ create table dbo.Product_Inventory(
 	quantity int not null default 0,
 	created_at datetime not null default getdate(),
 	last_modification datetime not null default getdate(),
-	deleted_state bit not null default 0
+	deleted_state bit not null default 0,
+	constraint FK_inventoryProduct foreign key (product_id)
+	references dbo.Product(product_id)
 )
 go
 
@@ -49,19 +53,34 @@ create table dbo.Person(
 )
 go
 
+create table dbo.Department(
+	department_id	INT Primary key identity(1,1),
+	department_name	VARCHAR(150) not null,
+	created_at datetime not null default getdate(),
+	last_modification datetime not null default getdate(),
+	deleted_state bit not null default 0
+)
+go
+
 create table dbo.Employee(
 	employee_id	INT Primary key identity(1,1),
 	person_id	INT not null, --foreign key
 	work_email	NVARCHAR(250) not null,
 	employee_number	INT not null, 
 	hire_date	DATE not null,
-	department_id	INt not null, --foreign key --se debe crear la tabla de 
+	department_id	INt not null, --foreign key
 	salary	DECIMAL(10,2) not null,
 	created_at datetime not null default getdate(),
 	last_modification datetime not null default getdate(),
-	deleted_state bit not null default 0
+	deleted_state bit not null default 0,
+	constraint FK_employeePerson foreign key (person_id)
+	references dbo.Person(person_id),
+	constraint FK_employeeDepartment foreign key (department_id)
+	references dbo.Department(department_id)
 )
 go
+
+
 
 create table dbo.Client(
 	client_id	INT Primary key identity(1,1),
@@ -75,15 +94,31 @@ create table dbo.Client(
 )
 go
 
+create table dbo.Payment_Details(
+	payment_id	INT Primary key identity(1,1),
+	order_id	INT not null, --foreign key --arreglar order_id
+	amount	DECIMAL(10,2)not null,
+	status	BIT not null,
+	created_at datetime not null default getdate(),
+	last_modification datetime not null default getdate()
+)
+go
+
 create table dbo.Order_Details(
 	details_id	INT Primary key identity(1,1), 
-	user_id	INT not null, --foreign key
+	client_id	INT not null, --foreign key
 	total	DECIMAL(10,2)not null,
 	payment_id	INT not null, --foreign key
 	employee_id	INT,--foreign key
 	created_at datetime not null default getdate(),
 	last_modification datetime not null default getdate(),
-	deleted_state bit not null default 0
+	deleted_state bit not null default 0,
+	constraint FK_orderClient foreign key (client_id)
+	references dbo.Client(client_id),
+	constraint FK_orderPayment foreign key (payment_id)
+	references dbo.Payment_Details(payment_id),
+	constraint FK_orderEmployee foreign key (employee_id)
+	references dbo.Employee(employee_id)
 )
 go
 
@@ -93,29 +128,26 @@ create table dbo.Order_Items(
 	quantity	INT not null,
 	unit_price	DECIMAL(10,2) not null,
 	created_at datetime not null default getdate(),
-	last_modification datetime not null default getdate()
+	last_modification datetime not null default getdate(),
 	--deleted_state bit not null default 0 --Debería de poder eliminarse los items de una orden
+	constraint FK_orderProducts foreign key (product_id)
+	references dbo.Product(product_id)
 )
 go
 
-create table dbo.Payment_Details(
-	payment_id	INT Primary key identity(1,1),
-	order_id	INT not null, --foreign key
-	amount	DECIMAL(10,2)not null,
-	status	BIT not null,
-	created_at datetime not null default getdate(),
-	last_modification datetime not null default getdate()
-)
-go
 
 create table dbo.Shopping_Cart(
 	cart_id	INT Primary key identity(1,1),
 	product_id	INT not null, --foreign key
-	user_id	DECIMAL(10,2)not null, --foreign key
+	client_id	int not null, --foreign key
 	quantity	INT not null,
 	created_at datetime not null default getdate(),
 	last_modification datetime not null default getdate(),
-	deleted_state bit not null default 0
+	deleted_state bit not null default 0,
+	constraint FK_cartProducts foreign key (product_id)
+	references dbo.Product(product_id),
+	constraint FK_cartClient foreign key (client_id)
+	references dbo.Client(client_id)
 )
 go
 
@@ -131,6 +163,8 @@ create table dbo.Person_Address(
 	mobile	VARCHAR(15),
 	created_at datetime not null default getdate(),
 	last_modification datetime not null default getdate(),
-	deleted_state bit not null default 0
+	deleted_state bit not null default 0,
+	constraint FK_adressPerson foreign key (person_id)
+	references dbo.Person(person_id)
 )
 go
